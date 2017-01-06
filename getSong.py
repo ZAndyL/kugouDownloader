1b95c928e4d0943f50e19369b64e5d04c64b0273
Andy Liang <zliang@slack-corp.com>
2017-01-05 22:51:17 -0500

    Download the songs

---
+++ getSong.py
@@ -0,0 +1,58 @@
+from __future__ import with_statement, print_function
+import sys
+import urllib2
+import json
+import hashlib
+
+
+keyword = ' '.join(sys.argv[1:])
+url = 'http://mobilecdn.kugou.com/api/v3/search/song?format=json&page=1&pagesize=20&keyword=%27{0}%27'.format(keyword)
+
+request = urllib2.Request(url)
+response = urllib2.urlopen(request).read()
+data = json.loads(response)
+
+if len(data['data']['info']) > 0:
+	print('Found the following songs, which one do you want?')
+else:
+	print('Couldn\'t find anything for', keyword)
+	sys.exit()
+
+print(data)
+count = 0
+for songInfo in data['data']['info']:
+	filename = songInfo['filename']
+	optionString = u'[{0}] {1}'.format(count, filename)
+	if len(songInfo['320hash']) > 0:
+		optionString += ' 320kbps'
+	print(optionString)
+	count = count + 1
+
+optionSelected = input()
+songHash = data['data']['info'][optionSelected]['320hash'] if len(data['data']['info'][optionSelected]['320hash']) else data['data']['info'][optionSelected]['hash']
+
+m = hashlib.md5()
+m.update(songHash + 'kgcloud')
+MD5Hash = m.hexdigest()
+
+url = 'http://trackercdn.kugou.com/i/?cmd=4&hash={0}&key={1}&pid=1&forceDown=0&vip=1'.format(songHash, MD5Hash)
+
+request = urllib2.Request(url)
+response = urllib2.urlopen(request).read()
+data = json.loads(response)
+
+filename = data['fileName']+'.mp3'
+mp3url = data['url']
+mp3file = urllib2.urlopen(mp3url)
+
+print('downloading...')
+with open(filename,'wb') as output:
+    while True:
+        buf = mp3file.read(65536)
+        if not buf:
+            break
+        output.write(buf)
+        print('.', end="")
+        sys.stdout.flush()
+
+print('done!')
